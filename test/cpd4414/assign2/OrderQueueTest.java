87add6480065fe0f657c2e8e5ba37918b26dbf40
Kuldeep Singh <c0648442@mylambton.ca>
2015-01-31 00:24:36 -0500

    Second Test Case Written

--- test/cpd4414/assign2/OrderQueueTest.java
+++ test/cpd4414/assign2/OrderQueueTest.java
@@ -63,6 +63,21 @@ public class OrderQueueTest {
         long result = order.getTimeReceived().getTime();
         assertTrue(Math.abs(result - expResult) < 1000);
     }
-    
+    @Test
+    public void testWhenCustomerIdDoesNotExistsAndCustomerNameDoesNotExistsThenThrowException() {
+        boolean choice = false;
+        OrderQueue orderQueue = new OrderQueue();
+        
+        Order order = new Order("", "");
+        order.addPurchase(new Purchase(0004, 450));
+        order.addPurchase(new Purchase(0006, 250));
+        try {
+            orderQueue.add(order);
+        } catch (Exception ex) {
+            choice = true;
+        }
+        assertTrue(choice);
+        
+    }
     
 }
